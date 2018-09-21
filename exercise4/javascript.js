d83e102268efefedf44c48bccd6e01bb1398e172
cmeyer <artgirlie26@gmail.com>
2018-09-20 22:48:59 -0500

    got it to change from the value in the text field

--- exercise4/javascript.js
+++ exercise4/javascript.js
@@ -1,12 +1,12 @@
-var paragraph= document.getElementById("changeMe");
-var border = document.getElementById("border").value;
-var background = document.getElementById("background").value;
-
 function changeBorderColor(){
   //document.write(border);
   //if((border == "red") || (border == "green") || (border == "blue")){
+  //var paragraph= document.getElementById("changeMe");
+  //var border = document.getElementById("border").value;
+  var back = document.getElementById("background").value;
+  alert(document.getElementById("background").value);
     alert("called it");
-    document.getElementById("changeMe").style.backgroundColor="blue";
+    document.getElementById("changeMe").style.backgroundColor=back;
   //}
   //else{
   //  alert("Must be red, green or blue");
