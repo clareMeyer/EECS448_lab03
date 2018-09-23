7f3895c0962f44a11103195fbf11fac13099a35b
cmeyer <artgirlie26@gmail.com>
2018-09-22 20:06:54 -0500

    added comments to exercise1

--- exercise1/java1.js
+++ exercise1/java1.js
@@ -1,3 +1,13 @@
+/*
+java1.js
+Author: Clare Meyer
+Lab: 03; Exercise: 01
+functions for index.html
+ */
+
+//checks if passwords are the same and at least 8 characters
+//called when validate button is hit from main.html
+//takes in text values from main.html
 function validate(){
   var passOne = document.getElementById("passOne").value;
   var passTwo = document.getElementById("passTwo").value;
